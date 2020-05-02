017c4366066eef6ab39ec0fa2cb34535a10e0ab3
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-05-02 14:58:47 -0400

    Add project files.

---
+++ ProyectoFinalAp2/Models/Clientes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAp2.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string RNC { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public DateTime Fecha { get; set; }
+        
+        public Clientes()
+        {
+            ClienteId = 0;
+            Nombres = string.Empty;
+            RNC = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+            Email = string.Empty;
+            Fecha = DateTime.Now;
+           
+        }
+    }
+}
